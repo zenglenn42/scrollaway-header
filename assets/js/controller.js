20ac6cffb411e2daca20fee0e6e766ada25b7859
Glenn Streiff <gsnospampls@pobox.com>
2019-05-20 15:36:51 -0500

    programmatically generate the landing page html

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -1,8 +1,24 @@
 function Controller(bodyDivId) {
   let bodyDiv = document.getElementById(bodyDivId);
-  let header = createHeader("CityMatch", "search");
+  let header = createHeader("City Match", "search");
+  let menuDrawer = createMenuDrawer("Settings", ["About", "Contact", "Help"]);
+  let hamburgerMenu = createHamburgerMenu();
+  let landingText1 =
+    "Considering a move but not sure which cities are your best bet?";
+  let landingText2 =
+    "Explore your options by sharing what's important to you and we'll offer some informed choices.";
+  let mainLanding = createMainLanding(
+    "Find your city",
+    landingText1,
+    landingText2
+  );
+  let footer = createFooter("navigate_next");
 
-  bodyDiv.append(header);
+  $(bodyDiv).append(header);
+  $(bodyDiv).append(menuDrawer);
+  $(bodyDiv).append(hamburgerMenu);
+  $(bodyDiv).append(mainLanding);
+  $(bodyDiv).append(footer);
 }
 
 function createHeader(title, rightNavIcon) {
@@ -16,4 +32,74 @@ function createHeader(title, rightNavIcon) {
     </div>
   `);
   return h;
+}
+
+function createMenuDrawer(title, menuItemsArray) {
+  md = document.createElement("div");
+  $(md).addClass("mdl-layout__drawer");
+  $(md).append(`
+    <span class="mdl-layout-title">${title}</span>
+  `);
+  nav = document.createElement("nav");
+  $(nav).addClass("mdl-navigation");
+  menuItemsArray.map(item => {
+    nav.append(`
+      <a class="mdl-navigation__link" href="">${item}</a>
+    `);
+  });
+  $(md).append(nav);
+  return md;
+}
+
+function createHamburgerMenu() {
+  m = document.createElement("div");
+  $(m).addClass("mdl-layout__drawer-button");
+  $(m).attr("role", "button");
+  $(m).attr("aria-expanded", "false");
+  $(m).append(`
+    <i class="material-icons">menu</i>
+  `);
+  return m;
+}
+
+function createMainLanding(titleText, supportText1, supportText2) {
+  m = document.createElement("main");
+  $(m).addClass("content");
+  $(m).attr("id", "main");
+  $(m).html(`
+    <div class="landing-card-wide mdl-card mdl-shadow--2dp">
+      <div class="mdl-card__title">
+        <h2 class="mdl-card__title-text">${titleText}</h2>
+      </div>
+      <div class="mdl-card__supporting-text">
+        <p style="line-height: 1.25em;">${supportText1}</p>
+        <p style="line-height: 1.25em;">${supportText2}</p>
+      </div>
+      <div class="mdl-card__actions mdl-card--border">
+        <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+          Get Started
+        </a>
+      </div>
+      <div class="mdl-card__menu">
+        <button class="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect"></button>
+      </div>
+    </div>    
+  `);
+  return m;
+}
+
+function createFooter(fabIcon) {
+  f = document.createElement("footer");
+  $(f).addClass("mdl-mini-footer");
+  $(f).html(`
+    <button id="navigate_next" data-nextpage="preferences" class="footer-fab mdl-button mdl-js-button mdl-button--fab mdl-js-ripple-effect mdl-button--mini-fab mdl-button--primary">
+      <i class="material-icons">${fabIcon}</i>
+    </button>
+    <div class="mdl-mini-footer__left-section"><span class="copyright-text">
+      <i class="material-icons footer-icons">location_city</i>
+      <i class="material-icons footer-icons">favorite</i>
+      <span class="copyright-text">Match Industries &copy; 2019</span>
+    </div>
+  `);
+  return f;
 }
