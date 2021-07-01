57d6ac4864a1c8a3358396a1058c4b83cf55c910
Glenn Streiff <gsnospampls@pobox.com>
2021-07-01 12:29:05 -0500

    code complete on landing page view model
    Removing some hardcoded strings from the view and pushing them to the model.
    
    The ModelLanding class is essentially a string table of locale-sensitive
    presentation text required by the splash screen.
    
    It provides getter access to things like:
    
      application name
      slogan
      call-to-action blurb
    
    I've coded it in a way that lends itself to simple localization if we want
    to support that later.

---
+++ assets/js/models/model-landing.js
@@ -0,0 +1,250 @@
+//----------------------------------------------------------------------------------
+// ModelLanding
+//
+// This class maintains state related to the application's landing page:
+//
+//    app name, 
+//    slogan,
+//    call-to-action blurb
+//----------------------------------------------------------------------------------
+// TODO: Make this observable (in the software patterns sense).
+// TODO: Decide if ctor should throw exception on invalid input versus resilient
+//       fallback to default values.
+//----------------------------------------------------------------------------------
+
+function ModelLanding(locale) {
+  this.dfltLocale = "en-US"
+  this.locale = (this.isValidLocale(locale)) ? locale : this.dfltLocale
+
+  this.msgCatalog = {
+    "en-US": {
+      appName: "City Match",
+      slogan: "Find your city",
+      blurb: "Thinking about a move but not sure which city is your best bet?\n" +
+                         "Share your priorities and we'll offer some options to consider."
+    }
+  }
+}
+
+ModelLanding.prototype.isValidLocale = function(locale) {
+  return (locale === "en-US")
+}
+
+ModelLanding.prototype.getLocale = function() {
+  if (this.isValidLocale(this.locale)) {
+    return this.locale
+  } else {
+    console.log("ModelLanding.getLocale() Info: returning default locale. Not", this.locale)
+    return this.dfltLocale
+  }
+}
+
+ModelLanding.prototype.setLocale = function(locale) {
+  let result = false
+  if (this.isValidLocale(locale)) {
+    this.locale = locale
+    result = true
+  } else {
+    console.log('Error ModelLanding.setLocale.  Invalid locale:', locale)
+  }
+  return result
+}
+
+ModelLanding.prototype.isValidLocaleProperty = function(locale, prop) {
+  return (this.msgCatalog.hasOwnProperty(locale)) &&
+         (this.msgCatalog[locale].hasOwnProperty(prop))
+}
+
+ModelLanding.prototype.getAppName = function() {
+  let result = "missing_appName"
+  if (this.isValidLocaleProperty(this.locale, 'appName')) {
+    result = this.msgCatalog[this.locale].appName
+  } else if (this.isValidLocaleProperty(this.dfltLocale, 'appName')) {
+    result = this.msgCatalog[this.dfltLocale].appName
+  } else {
+    result = (this.locale) ? result + "_" + this.locale : result
+    console.log("ModelLanding:getAppName() Error ", result)
+  }
+  return result
+}
+
+ModelLanding.prototype.getSlogan = function() {
+  let result = "missing_slogan"
+  if (this.isValidLocaleProperty(this.locale, 'slogan')) {
+    result = this.msgCatalog[this.locale].slogan
+  } else if (this.isValidLocaleProperty(this.dfltLocale, 'slogan')) {
+    result = this.msgCatalog[this.dfltLocale].slogan
+  } else {
+    result = (this.locale) ? result + "_" + this.locale : result
+    console.log("ModelLanding:getSlogan() Error ", result)
+  }
+  return result
+}
+
+ModelLanding.prototype.getBlurb = function() {
+  let result = "missing_blurb"
+  if (this.isValidLocaleProperty(this.locale, 'blurb')) {
+    result = this.msgCatalog[this.locale].blurb
+  } else if (this.isValidLocaleProperty(this.dfltLocale, 'blurb')) {
+    result = this.msgCatalog[this.dfltLocale].blurb
+  } else {
+    result = (this.locale) ? result + "_" + this.locale : result
+    console.log("ModelLanding:getBlurb() Error ", result)
+  }
+  return result
+}
+
+//----------------------------------------------------------------------------------
+// Unit Test
+//----------------------------------------------------------------------------------
+
+function mkFailMsg(codeUnderTest, failStr) {
+  let failMsg = `  [fail] ${codeUnderTest} \n  ${failStr}`
+  return failMsg
+}
+
+function mkPassMsg(codeUnderTest, iterations) {
+  let passMsg = ` [pass] ${codeUnderTest}`
+  if (iterations) {
+    passMsg += ` (${iterations} iterations)`
+  }
+  return passMsg
+}
+
+function UnitTestModelLanding() {
+  let cut = ""
+  let failure = undefined
+  let errmsg = undefined
+  let module = "model-landing.js"
+
+  console.log('Starting unit tests for ' + module)
+  console.log('---------------------------------------')
+
+
+  // Test #1
+  try {
+    let dfltLandingModel = new ModelLanding()
+    cut = "ModelLanding() dflt constructor"
+    console.log(' Verifying default construction when ctor called without params ...')
+
+    if (dfltLandingModel.locale !== "en-US") {
+      failure = "Expected locale of en-US, but got " + dfltLandingModel.locale
+    }
+
+    if (failure) {
+      throw(mkFailMsg(cut, failure))
+    } else {
+      console.log(mkPassMsg(cut))
+    }
+  } catch(e) {
+    console.error(e)
+  }
+
+  // Test #2
+  try {
+    failure = ""
+    let nrmlLandingModel = new ModelLanding("en-US")
+    cut = "ModelLanding('en-US') constructor"
+    console.log(' Verifying nominal construction when ctor called with valid locale ...')
+
+    if (nrmlLandingModel.locale !== "en-US") {
+      failure = "Expected locale of en-US, but got " + nrmlLandingModel.locale
+    }
+
+    if (failure) {
+      throw(mkFailMsg(cut, failure))
+    } else {
+      console.log(mkPassMsg(cut))
+    }
+  } catch(e) {
+    console.error(e)
+  }
+
+  // Test #3
+  try {
+    failure = ""
+    let failLandingModel = new ModelLanding("bogus-locale")
+    cut = "ModelLanding('bogus-locale') constructor"
+
+    // TODO: Should ctor throw exception or resiliently fallback to default construction?
+    console.log(' Verifying fallback construction when ctor called with invalid locale ...')
+
+    if (failLandingModel.locale !== "en-US") {
+      failure = "Expected fallback locale of en-US, but got " + failLandingModel.locale
+    }
+
+    if (failure) {
+      throw(mkFailMsg(cut, failure))
+    } else {
+      console.log(mkPassMsg(cut))
+    }
+  } catch(e) {
+    console.error(e)
+  }
+
+  // Test #4
+  let landingModel = new ModelLanding("en-US")
+  try {
+    failure = ""
+    cut = "ModelLanding.getAppName()"
+    console.log(' Verifying expected application name is returned from getter ...')
+
+    let appName = landingModel.getAppName()
+
+    if (appName !== "City Match") {
+      failure = "Expected appName of 'City Match', but got " + appName
+    }
+
+    if (failure) {
+      throw(mkFailMsg(cut, failure))
+    } else {
+      console.log(mkPassMsg(cut))
+    }
+  } catch(e) {
+    console.error(e)
+  }
+
+  // Test #5
+  try {
+    failure = ""
+    cut = "ModelLanding.getSlogan()"
+    console.log(' Verifying expected slogan is returned from getter ...')
+
+    let slogan = landingModel.getSlogan()
+
+    if (slogan !== "Find your city") {
+      failure = "Expected slogan of 'Find your city', but got " + slogan
+    }
+
+    if (failure) {
+      throw(mkFailMsg(cut, failure))
+    } else {
+      console.log(mkPassMsg(cut))
+    }
+  } catch(e) {
+    console.error(e)
+  }
+
+  // Test #6
+  try {
+    failure = ""
+    cut = "ModelLanding.getBlurb()"
+    console.log(' Verifying expected blurb is returned from getter ...')
+
+    let actualBlurb = landingModel.getBlurb()
+    let expectedBlurb = "Thinking about a move but not sure which city is your best bet?\n" +
+                         "Share your priorities and we'll offer some options to consider."
+
+    if (actualBlurb !== expectedBlurb) {
+      failure = "Didn't get expected blurb.  Got " + actualBlurb
+    }
+
+    if (failure) {
+      throw(mkFailMsg(cut, failure))
+    } else {
+      console.log(mkPassMsg(cut))
+    }
+  } catch(e) {
+    console.error(e)
+  }
+}
